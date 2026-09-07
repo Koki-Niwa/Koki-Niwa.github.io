a3225e22e89efe0ad8663833362c96c0516215a3
Koki <koki@example.com>
2026-09-07 15:42:06 +0800

    Fix: rename InkBackground to .js, restart dev server

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import InkBackground from '@site/src/components/InkBackground';
+import InkBackground from '@site/src/components/InkBackground.js';
 import Heading from '@theme/Heading';
 
 function HomepageHeader() {
